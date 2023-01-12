1de971174cf9aaeb30687859f8aff4ae95cb1cbf
lihong <815958557@qq.com>
2023-01-12 18:00:50 +0800

    在application.properties种定义了数据源类型

--- src/main/java/com/univers/basic/Constant_Interface.java
+++ src/main/java/com/univers/basic/Constant_Interface.java
@@ -1,6 +1,9 @@
 package com.univers.basic;
 /*
 * 使用接口创建常量，无需添加上static final关键字，接口被编译过后会自动被加上的
+* 常量接口存在的一些缺陷：
+* 1.可被子类继承，并修改常量的定义
+* 2.
 * */
 public interface Constant_Interface {
     String constant="demotest_1";
